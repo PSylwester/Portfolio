7532503da3fcf497d0a93a5bbd0ee22629c26ebc
PSylwester <p.sylwek18@wp.pl>
2026-05-05 16:31:09 +0200

    update styles #2 add scroll to top for router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,13 +2,17 @@ import { FunctionComponent } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 import { Home, About, Contact } from '@/pages';
+import { ScrollToTop } from './ScrollToTop';
 
 export const Router: FunctionComponent = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="About" element={<About />} />
-      <Route path="Contact" element={<Contact />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="About" element={<About />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 };
