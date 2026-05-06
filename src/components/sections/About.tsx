e6d6469bbbff9c1f73bfb095a58602d725f176ba
PSylwester <p.sylwek18@wp.pl>
2026-05-06 18:02:46 +0200

    update styles #3

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -45,13 +45,6 @@ const experiences: Experience[] = [
 export const About: FunctionComponent = () => {
   return (
     <div className="relative min-h-screen ">
-      {/* Background decorative elements */}
-      {/* <div className="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none">
-        <div className="absolute top-20 left-10 w-72 h-72 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full mix-blend-screen filter blur-xl opacity-30 animate-blob"></div>
-        <div className="absolute top-40 right-10 w-72 h-72 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full mix-blend-screen filter blur-xl opacity-30 animate-blob animation-delay-2000"></div>
-        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-gradient-to-r from-pink-500 to-blue-500 rounded-full mix-blend-screen filter blur-xl opacity-30 animate-blob animation-delay-4000"></div>
-      </div> */}
-
       <main className="relative min-h-screen py-16 px-4">
         {/* Header Section */}
         <section className="text-center mb-16 flex flex-col items-center justify-center">
